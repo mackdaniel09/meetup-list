0dd1b350359b93829872fbd8407c5de99d0431fa
mackdaniel09 <mackdaniel06@gmail.com>
2022-01-20 08:38:44 +0800

    completing the project

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,25 @@
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 
 function NewMeetups() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetup-list-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add new Meetups</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
